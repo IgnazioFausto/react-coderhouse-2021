cf1ba51c1152b73799c4ae125b9113e37a0caae9
IgnacioF <nachojam777@gmail.com>
2021-09-19 21:33:07 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="titulo">Mi Huertapp</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ingresa
         </a>
       </header>
     </div>
